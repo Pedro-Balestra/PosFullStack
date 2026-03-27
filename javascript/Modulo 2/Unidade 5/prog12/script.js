19ae204bf5ea468a27c9a0e9bc8815a1068c3d8f
Pedro Balestra <pedro.balestra@outlook.com>
2026-03-27 20:08:58 -0300

    Console

---
+++ javascript/Modulo 2/Unidade 5/prog12/script.js
@@ -0,0 +1,28 @@
+// (function () {
+//   console.log('mensagem log');
+//   console.info('mensagem info');
+//   console.warn('mensagem warning');
+//   console.error('mensagem error');
+//   console.debug('mensagem debug');
+// })();
+
+// console.log("%cOlá %s, hoje é dia %d", "color: rgba(255,135,135,0.8); font-size: large", "João", 27);
+
+// console.table([1, 2, 3, 4, 5]);
+// console.table([
+//   { nome: "João", idade: 27, cidade: "São Paulo" },
+//   { nome: "Maria", idade: 30, cidade: "Rio de Janeiro" },
+//   { nome: "Pedro", idade: 35, cidade: "Brasília" }
+// ]);
+
+// console.count("a")
+// console.count("a")
+// console.count("ana")
+// console.count("a")
+// console.count("ana")
+
+// console.time("tempo")
+// for (let i = 0; i < 1000000; i++) { }
+// console.timeEnd("tempo")
+
+console.log(console.memory);

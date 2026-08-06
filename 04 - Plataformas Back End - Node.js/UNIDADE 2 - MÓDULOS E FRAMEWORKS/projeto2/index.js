ff4c6fea114e5ae14a705ebb4bbe1d73e13ed869
Pedro Balestra <pedro.balestra@outlook.com>
2026-08-06 18:08:52 -0300

    2.5. Express framework - Parte 2

--- 04 - Plataformas Back End - Node.js/UNIDADE 2 - MÓDULOS E FRAMEWORKS/projeto2/index.js
+++ 04 - Plataformas Back End - Node.js/UNIDADE 2 - MÓDULOS E FRAMEWORKS/projeto2/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 
-mid = (req, res) => {
-    res.send('Hello World');
-}
+app.get('/', (req, res, next) => {
+    console.log(new Date().toLocaleDateString(), req.method, req.path);
+    next();
+});
 
-app.get('/', mid)
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
-})
+});

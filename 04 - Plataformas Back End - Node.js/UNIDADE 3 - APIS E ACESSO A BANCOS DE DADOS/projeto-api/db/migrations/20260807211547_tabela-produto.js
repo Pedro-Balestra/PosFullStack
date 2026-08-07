4a7d1a3587714a249a184715634dec39c304d6e6
Pedro Balestra <pedro.balestra@outlook.com>
2026-08-07 19:18:03 -0300

    3.9. Criando API - Estágio 2 - Parte 2

--- 04 - Plataformas Back End - Node.js/UNIDADE 3 - APIS E ACESSO A BANCOS DE DADOS/projeto-api/db/migrations/20260807211547_tabela-produto.js
+++ 04 - Plataformas Back End - Node.js/UNIDADE 3 - APIS E ACESSO A BANCOS DE DADOS/projeto-api/db/migrations/20260807211547_tabela-produto.js
@@ -5,7 +5,7 @@
 exports.up = function (knex) {
     return knex.schema.createTable('produtos', table => {
         table.increments('id').primary();
-        table.string('descricao', 255).notNullable();
+        table.string('descricao', 255).unique().notNullable();
         table.string('marca', 128).notNullable();
         table.decimal('preco', 10, 2).notNullable();
     });
--- 04 - Plataformas Back End - Node.js/UNIDADE 3 - APIS E ACESSO A BANCOS DE DADOS/projeto-api/routes/apiRouterV2.js
+++ 04 - Plataformas Back End - Node.js/UNIDADE 3 - APIS E ACESSO A BANCOS DE DADOS/projeto-api/routes/apiRouterV2.js
@@ -16,12 +16,18 @@ apiRouterV2.get('/produtos/:id', function (req, res, next) {
   let id = req.params.id;
   if (id) {
     idInt = Number.parseInt(id);
-    let idx = produtos.findIndex(p => p.id === idInt);
-    if (idx > -1) {
-      res.json(produtos[idx]);
-    } else {
-      res.status(404).json({ message: 'Produto não encontrado' });
-    }
+    knex('produtos').select('*').where({ id: idInt }).then(produtos => {
+      if (!produtos.length) {
+        res.status(404).json({ message: 'Produto não encontrado' });
+        return;
+      }
+      let produto = produtos[0];
+      res.status(200).json(produto);
+
+    })
+      .catch(err => {
+        res.status(500).json({ message: `Erro ao buscar produto ${err.message}` });
+      });
   } else {
     res.status(404).json({ message: 'Produto não encontrado' });
   }
@@ -29,23 +35,34 @@ apiRouterV2.get('/produtos/:id', function (req, res, next) {
 
 apiRouterV2.post('/produtos', function (req, res, next) {
   let produto = req.body;
-  let newId = Math.max(...produtos.map(p => p.id)) + 1;
-  produto.id = newId;
-  produtos.push(produto);
-  res.status(201).json({ message: 'Produto criado com sucesso', data: { id: newId } });
+  knex('produtos').insert(produto, ['id']).then(produtos => {
+    if (!produtos.length) {
+      res.status(400).json({ message: 'Erro ao criar produto' });
+      return;
+    } else {
+      let id = produtos[0].id;
+      res.status(201).json({ message: 'Produto criado com sucesso', data: { id: id } });
+    }
+  })
+    .catch(err => {
+      res.status(500).json({ message: `Erro ao criar produto ${err.message}` });
+    });
 })
 
 apiRouterV2.delete('/produtos/:id', function (req, res, next) {
   let id = req.params.id;
   if (id) {
     idInt = Number.parseInt(id);
-    let idx = produtos.findIndex(p => p.id === idInt);
-    if (idx > -1) {
-      produtos.splice(idx, 1);
-      res.status(200).json({ message: 'Produto removido com sucesso' });
-    } else {
-      res.status(404).json({ message: 'Produto não encontrado' });
-    }
+    knex('produtos').where({ id: idInt }).del().then(result => {
+      if (result === 0) {
+        res.status(404).json({ message: 'Produto não encontrado' });
+      } else {
+        res.status(200).json({ message: 'Produto removido com sucesso' });
+      }
+    })
+      .catch(err => {
+        res.status(500).json({ message: `Erro ao remover produto ${err.message}` });
+      });
   } else {
     res.status(404).json({ message: 'Produto não encontrado' });
   }
@@ -56,15 +73,16 @@ apiRouterV2.put('/produtos/:id', function (req, res, next) {
   let produto = req.body;
   if (id) {
     idInt = Number.parseInt(id);
-    let idx = produtos.findIndex(p => p.id === idInt);
-    if (idx > -1) {
-      produtos[idx].descricao = produto.descricao;
-      produtos[idx].marca = produto.marca;
-      produtos[idx].preco = produto.preco;
-      res.status(200).json({ message: 'Produto atualizado com sucesso', data: produtos[idx] });
-    } else {
-      res.status(404).json({ message: 'Produto não encontrado' });
-    }
+    knex('produtos').where({ id: idInt }).update(produto).then(result => {
+      if (result === 0) {
+        res.status(404).json({ message: 'Produto não encontrado' });
+      } else {
+        res.status(200).json({ message: 'Produto atualizado com sucesso', data: { ...produto, id: idInt } });
+      }
+    })
+      .catch(err => {
+        res.status(500).json({ message: `Erro ao atualizar produto ${err.message}` });
+      });
   } else {
     res.status(404).json({ message: 'Produto não encontrado' });
   }

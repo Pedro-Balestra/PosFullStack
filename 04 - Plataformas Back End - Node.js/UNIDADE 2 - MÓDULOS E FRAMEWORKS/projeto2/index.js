2e96a994b91b7dbab672760269953eb33367f560
Pedro Balestra <pedro.balestra@outlook.com>
2026-08-07 16:21:22 -0300

    2.7. Express framework - Parte 4

--- 04 - Plataformas Back End - Node.js/UNIDADE 2 - MÓDULOS E FRAMEWORKS/projeto2/index.js
+++ 04 - Plataformas Back End - Node.js/UNIDADE 2 - MÓDULOS E FRAMEWORKS/projeto2/index.js
@@ -11,9 +11,40 @@ app.use((req, res, next) => {
 app.use(express.static('public'));
 
 app.post('/users', (req, res) => {
+    console.log(req.get('content-type'));
     dados = req.body;
     console.log(dados);
     res.send(`Dados recebidos com sucesso! Seja bem-vindo, ${dados.nome}!`);
+});
+
+// app.get('/users/:nome', (req, res) => {
+//     const { nome } = req.params;
+//     const { cidade } = req.query;
+//     res.send(`Ola, ${nome}! Você é da cidade de ${cidade}.`);
+// });
+
+app.get('/users/site', (req, res) => {
+    res.redirect('/site');
+});
+
+
+app.get('/dados', (req, res) => {
+
+    console.log(req.get('accept'));
+
+    // res.format({
+    //     'text/html': () => {
+    //         res.send('<h1>Olá</h1>');
+    //     },
+    //     'application/json': () => {
+    //         res.json({ message: 'Olá' });
+    //     }
+    // });
+    if (req.accepts('application/json')) {
+        res.json({ message: 'Olá' });
+    } else {
+        res.send('<h1>Olá</h1>');
+    }
 });
 
 app.listen(3000, () => {

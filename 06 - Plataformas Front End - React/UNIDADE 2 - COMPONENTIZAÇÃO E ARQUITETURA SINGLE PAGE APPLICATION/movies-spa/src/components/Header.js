973bf0cc8d641383bbfe49ab7a9314cd710165a5
Pedro Balestra <pedro.balestra@outlook.com>
2026-08-14 18:03:15 -0300

    Unidade 2 - 6. Arquitetura Single Page Application - Parte 2

---
+++ 06 - Plataformas Front End - React/UNIDADE 2 - COMPONENTIZAÇÃO E ARQUITETURA SINGLE PAGE APPLICATION/movies-spa/src/components/Header.js
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <>
+            <h1>Aplicação de Filmes</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/movies">Movies</a></li>
+            </ul>
+        </>
+    )
+}

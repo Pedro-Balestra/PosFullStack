3a70cacd60910831f4f491fa3ed945569b6f6a75
Pedro Balestra <pedro.balestra@outlook.com>
2026-08-13 21:40:58 -0300

    Unidade 1 - 7. React hooks - useState()

--- 06 - Plataformas Front End - React/UNIDADE 1 - INTRODUÇÃO/app01/src/App.js
+++ 06 - Plataformas Front End - React/UNIDADE 1 - INTRODUÇÃO/app01/src/App.js
@@ -1,13 +1,19 @@
 
+import { useState } from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { SkillsList } from './components/SkillsList';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  console.log("Renderizando App...");
+
   return (
     <div className="App">
       <Header firstName="Pedro" />
       <SkillsList skills={['HTML', 'CSS', 'JavaScript', 'React']} />
+      <button onClick={() => setLoading(true)}>{loading ? 'Loading...' : 'Load Skills'}</button>
     </div>
   );
 }

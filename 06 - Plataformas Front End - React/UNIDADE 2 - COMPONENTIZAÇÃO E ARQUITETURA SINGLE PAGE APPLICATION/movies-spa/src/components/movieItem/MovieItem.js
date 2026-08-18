bb9c5d6e7134925a928ae21f8bc7f241d184fe37
Pedro Balestra <pedro.balestra@outlook.com>
2026-08-17 21:40:54 -0300

    Unidade 3 - 6. Implementando o Redux na aplicação de filmes (parte 4)

--- 06 - Plataformas Front End - React/UNIDADE 2 - COMPONENTIZAÇÃO E ARQUITETURA SINGLE PAGE APPLICATION/movies-spa/src/components/movieItem/MovieItem.js
+++ 06 - Plataformas Front End - React/UNIDADE 2 - COMPONENTIZAÇÃO E ARQUITETURA SINGLE PAGE APPLICATION/movies-spa/src/components/movieItem/MovieItem.js
@@ -1,22 +1,43 @@
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { addMovie } from '../../store/reducers/favorito';
-import styles from './MovieItem.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { addMovie, removeMovie } from "../../store/reducers/favorito";
+import styles from "./MovieItem.module.css";
 
 export function MovieItem({ movie }) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.favorito.movies);
 
+  function renderButton() {
+    if (movies.find((m) => m.id === movie.id)) {
+      return (
+        <button onClick={() => dispatch(removeMovie(movie))}>
+          Remover dos favoritos
+        </button>
+      );
+    }
     return (
-        <div className={styles.movie}>
-            {movie.poster_path && (
-                <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt="" />
-            )}
+      <button onClick={() => dispatch(addMovie(movie))}>
+        Adcionar aos favoritos
+      </button>
+    );
+  }
 
-            <h3>{movie.title}</h3>
-            <p>
-                <Link className={styles.detailsBtn} to={`/movies/${movie.id}`}>Ver detalhes</Link>
-                <button onClick={() => dispatch(addMovie(movie))}>Adcionar aos favoritos</button>
-            </p>
-        </div>
-    )
+  return (
+    <div className={styles.movie}>
+      {movie.poster_path && (
+        <img
+          src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+          alt=""
+        />
+      )}
+
+      <h3>{movie.title}</h3>
+      <p>
+        <Link className={styles.detailsBtn} to={`/movies/${movie.id}`}>
+          Ver detalhes
+        </Link>
+        {renderButton()}
+      </p>
+    </div>
+  );
 }
